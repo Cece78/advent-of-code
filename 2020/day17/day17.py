155f6953674fbea5ba1191af8c6da50f7d3b1a6e
Cece78 <45494688+Cece78@users.noreply.github.com>
2020-12-21 13:35:32 +0000

    Day17 part 2

--- 2020/day17/day17.py
+++ 2020/day17/day17.py
@@ -65,4 +65,66 @@ def run(data, n):
     return len(active_cubes)
     
 
-print(run(puzzle, 6))
+print("Part 1: ", run(puzzle, 6))
+
+
+# Part 2
+
+def create_positions(data):
+    active = set()
+    for x, row in enumerate(data):
+        for y, col in enumerate(row):
+            if col == '#':
+                active.add((x, y, 0, 0))
+    return active
+
+
+def count_neighbours(position, active):
+    x, y, z, w = position
+    count = 0
+    for i in [x-1, x, x+1]:
+        for j in [y-1, y, y+1]:
+            for k in [z-1, z, z+1]:
+                for l in [w-1, w, w+1]:
+                    if (i, j, k, l) in active and (i, j, k, l) != position:
+                        count+=1
+    return count
+ 
+    
+def generate_cubes(active):
+    cubes = set()
+    min_x, max_x = min(active, key = lambda t: t[0])[0], max(active, key = lambda t: t[0])[0]
+    min_y, max_y = min(active, key = lambda t: t[1])[1], max(active, key = lambda t: t[1])[1]
+    min_z, max_z = min(active, key = lambda t: t[2])[2], max(active, key = lambda t: t[2])[2]
+    min_w, max_w = min(active, key = lambda t: t[3])[3], max(active, key = lambda t: t[3])[3]
+    
+    for x in range(min_x-1, max_x+2):
+        for y in range(min_y-1, max_y+2):
+            for z in range(min_z-1, max_z+2):
+                for w in range(min_w-1, max_w+2):
+                    cubes.add((x, y, z, w))
+    return cubes
+    
+
+def update_state(active):
+    new_active = active.copy()  
+    for position in generate_cubes(active):
+        # Active cubes
+        if position in active:
+            if count_neighbours(position, active) <2 or count_neighbours(position, active)>3:
+                new_active.remove(position)
+        # Inactive cubes
+        else:
+            if count_neighbours(position, active) == 3:
+                new_active.add(position)
+    return new_active
+
+
+def run(data, n):
+    active_cubes = create_positions(data)
+    for i in range(n):
+        active_cubes = update_state(active_cubes)
+    return len(active_cubes)
+    
+
+print("Part 2: ", run(puzzle, 6))
